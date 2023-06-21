93aa1ce65d9cc37024113baff8f8cfac1b511036
Gabriel Messias Costa <gmessiasc@gmail.com>
2023-06-21 10:18:23 -0300

    feat - initializing domain

---
+++ src/LABCC.BackEnd.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace LABCC.BackEnd.Domain.Entities;
+
+internal abstract class BaseEntity
+{
+  public virtual long Id { get; set; }
+  public virtual long UpdatedAt { get; set; }
+  public virtual long CreatedAt { get; set; }
+  public virtual bool IsActive { get; set; }
+
+  public BaseEntity() {
+    var now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+    UpdatedAt = now;
+    CreatedAt = now;
+    IsActive = true;
+
+  }
+}
